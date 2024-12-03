5f857f9bb7fc80581a092e7aacfe09c566b1c4f8
Kaustubh <kaustubhupadhyay1@gmail.com>
2024-12-03 23:58:53 +0530

    heroku is no longer free

--- tapi/tapi/settings.py
+++ tapi/tapi/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,4 +139,3 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-django_heroku.settings(locals())
